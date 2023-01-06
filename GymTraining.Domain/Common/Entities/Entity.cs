4dea4665be1ebaa4a2c88b998b64a09b1b3ef763
Felipe Fogaça <felipe.foogaca@gmail.com>
2023-01-06 17:49:48 -0300

    added: entity common type

---
+++ GymTraining.Domain/Common/Entities/Entity.cs
@@ -0,0 +1,17 @@
+﻿namespace GymTraining.Domain.Common.Entities
+{
+    public class Entity
+    {
+        public Guid Id { get; set; }
+
+        public Entity(Guid id)
+        {
+            if(id == Guid.Empty)
+            {
+                id = Guid.NewGuid();
+            }
+
+            Id = id;
+        }
+    }
+}
