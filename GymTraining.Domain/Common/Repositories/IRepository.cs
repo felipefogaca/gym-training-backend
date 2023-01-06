07859cb931efd5b7d5f13e1421267347af0543b2
Felipe Fogaça <felipe.foogaca@gmail.com>
2023-01-06 17:53:27 -0300

    added: repository interface

---
+++ GymTraining.Domain/Common/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using GymTraining.Domain.Common.Entities;
+
+namespace GymTraining.Domain.Common.Repositories
+{
+    public interface IRepository<T> where T : Entity
+    {
+        Task<T> Find(Guid id);
+        Task Create(T entity);
+        Task Update(T entity);
+    }
+}
